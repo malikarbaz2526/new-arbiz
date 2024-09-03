e2e5f582a8486c769dfdbe65f91459adc4eadd8b
Arbaz Liaquatl <ak7490418@gmail.com>
2024-09-03 14:02:22 +0300

    eight

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,5 @@
 import React, {useEffect} from 'react'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import HeroBanner from './heroBanner/HeroBanner.jsx';
-=======
 import HeroBanner from './heroBanner/HeroBanner';
->>>>>>> d30a825f6c34c84280560eaa9860f56356ce9637
-=======
-import HeroBanner from './heroBanner/heroBanner';
->>>>>>> 6d81b5242f168cef7cd9667b727c805ea1cb3c1d
 import FoodCategory from './foodCategory/FoodCategory';
 import ProductArea from './productArea/ProductArea';
 import DealArea from './dealArea/DealArea';
